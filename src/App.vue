f646e5618a36bd1ed79bc774b68bb2f2b0712b49
juanr <juan.rauda4@gmail.com>
2024-09-07 08:51:59 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Mi app</h1>
+  <hr />
+  <MiComponente />
+  <!-- <MiComponente /> -->
+</template>
+
+<script setup lang="ts">
+import MiComponente from './components/MiComponente.vue'
+// import MiComponente from './components/MiComponente.vue'
+</script>
+
+<style scoped>
+h1 {
+  margin-left: 5px;
+}
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Primer componente</h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <section>
+    <h3>Número - {{ numeroCalculado }}</h3>
+  </section>
+  <button class="btn" @click="incrementar">+1</button>
+  <button class="btn" @click="decrementar">-1</button>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"></button>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const numero = ref(0)
+
+const numeroCalculado = computed(() => {
+  return `El número actual es: ${numero.value}`
+})
+
+const incrementar = () => {
+  numero.value++
+}
+
+const decrementar = () => {
+  numero.value--
+}
+</script>
+
+<style scoped>
+h1 {
+  margin-left: 5px;
+}
+h3 {
+  color: red;
+  margin-left: 5px;
+}
+</style>
